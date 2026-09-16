ff83758d444eac80ce5a07cbf3bb63ebc654247b
Isidro Sandoval Admin <isadmin@usurf.usu.edu>
2026-09-16 17:14:38 -0600

    updating path

--- initial.ps1
+++ initial.ps1
@@ -10,7 +10,7 @@
 #>
 
 $ErrorActionPreference = 'Stop'
-$Destination = "C:\ProgramData\SDL\scripts"
+$Destination = "C:\ProgramData\SDL\scripts\appattach"
 $TempPath = "C:\Temp\SoftwareInstall"
 $RepoZipUrl = "https://github.com/isg187/appattach/archive/refs/heads/main.zip"
 $InstallDir = Join-Path $Destination "install"
